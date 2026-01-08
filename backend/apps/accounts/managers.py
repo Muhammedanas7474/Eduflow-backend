45df753071d7b17e153c55e74c7ad955b34cf4b5
anas <muhammedanas247474@gmail.com>
2026-01-08 12:50:42 +0530

    Day 1: Django foundation with multi-tenant & MFA-ready structure

---
+++ backend/apps/accounts/managers.py
@@ -0,0 +1,26 @@
+from apps.tenants.models import Tenant
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, tenant, password=None):
+        if not phone_number:
+            raise ValueError("Phone number is required")
+
+        #  convert tenant ID → Tenant instance
+        if isinstance(tenant, int):
+            tenant = Tenant.objects.get(id=tenant)
+
+        user = self.model(
+            phone_number=phone_number,
+            tenant=tenant,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, tenant, password):
+        user = self.create_user(phone_number, tenant, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
---
+++ backend/apps/accounts/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from apps.tenants.models import Tenant
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    phone_number = models.CharField(max_length=15, unique=True)
+    tenant = models.ForeignKey(
+        Tenant,
+        on_delete=models.CASCADE,
+        related_name="users"
+    )
+
+    is_phone_verified = models.BooleanField(default=False)
+    mfa_enabled = models.BooleanField(default=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["tenant"]
+
+    def __str__(self):
+        return self.phone_number
